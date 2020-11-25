8599df48923471c459f6f550bfc4d54759341b99
Isabel Cachola <isabelcachola@utexas.edu>
2020-11-24 21:31:31 -0500

    edit preprocessing script for new data format

--- scripts/preprocess_tweets.py
+++ scripts/preprocess_tweets.py
@@ -12,6 +12,7 @@ import texthero as hero
 import glob
 import tqdm
 import csv
+import json
 
 def getSentimentScore(text, type='compound'):
     sid = SentimentIntensityAnalyzer()
@@ -22,56 +23,63 @@ def preprocess_tweets(datadir):
     files = glob.glob(os.path.join(os.path.abspath(datadir), "*.tsv"))
     logging.info(f'Found {len(files)} files')
 
-    # Read in total list of tweet ids and convert to int
-    # This is useful to make sure you have all of the data
-    total_id_list = list(map(lambda x: int(x.strip()), open(os.path.join(os.path.abspath(datadir), "all_tweet_ids.txt")).readlines()))
-
     dfs = []
 
-    for f in tqdm.tqdm(files):
+    for f in tqdm.tqdm(files, desc="Reading data"):
         dfs.append(pd.read_csv(f, sep='\t'))
     
     raw_data = pd.concat(dfs).drop_duplicates(subset=['id'])
-
-    if len(raw_data) != len(total_id_list):
-        logging.warning(f'Length of id list ({len(id_list)}) does not match number of tweets ({len(raw_data)}). \
-            Make sure you have the most recent list of ids and data files.')
+    # before = len(raw_data)
+    # raw_data.dropna(inplace=True, subset=['entities'])
+    # after = len(raw_data)
+    # logging.info(f'Dropped {before-after} rows with NA values. len(raw_data)={len(raw_data)}')
 
     id_list = []
     text_list = []
     author_id_list = []
-    author_num_followers_list = []
+    # author_num_followers_list = []
     retweet_count_list = []
     reply_count_list = []
     like_count_list = []
     quote_count_list = []
     mentions_list = []
     hashtags_list = []
+    label_list = []
 
-    for row_idx, row in raw_data.iterrows():
+    for row_idx, row in tqdm.tqdm(raw_data.iterrows(), 'Formatting data', total=len(raw_data)):
         #Getting new info
         cur_id = row['id']
 
         # Remove special characters
         formattex_text = re.sub(r"[^a-zA-Z0-9]+", ' ', row['text'])
 
-        cur_author_id = row['user-id']
-        curr_author_num_followers = row['user-followers_count']
-        cur_retweet_count = row['retweet_count']
-        cur_reply_count = row['reply_count']
-        cur_like_count = row['favorite_count']
-        cur_quote_count = row['quote_count']
+        row['public_metrics'] = eval(row['public_metrics'])
+        # import ipdb;ipdb.set_trace()
+        try:
+            row['entities'] = eval(row['entities'])
+        except:
+            row['entities'] = []
+
+        # import ipdb; ipdb.set_trace()
+        cur_author_id = row['author_id']
+        # curr_author_num_followers = row['public_metrics']['user-followers_count']
+        cur_retweet_count = row['public_metrics']['retweet_count']
+        cur_reply_count = row['public_metrics']['reply_count']
+        cur_like_count = row['public_metrics']['like_count']
+        cur_quote_count = row['public_metrics']['quote_count']
+        curr_label = 1 if (cur_reply_count > cur_retweet_count) else 0
         cur_mentions = []
         try:
-            for mention in eval(row['entities-user_mentions']):
-                cur_mentions = cur_mentions + [mention['username']]
+            for mention in row['entities']['mentions']:
+                cur_mentions.append(mention['username'])
         except:
             cur_mentions = []
 
         cur_hashtags = ""
+        # import ipdb;ipdb.set_trace()
         try:
-            for hashtag in eval(row['entities-hashtags']):
-                cur_hashtags = cur_hashtags + " " + hashtag['text']
+            for hashtag in row['entities']['hashtags']:
+                cur_hashtags = cur_hashtags + " " + hashtag['tag']
         except:
             cur_hashtags = ""
 
@@ -85,25 +93,30 @@ def preprocess_tweets(datadir):
         quote_count_list.append(cur_quote_count)
         mentions_list.append(cur_mentions)
         hashtags_list.append(cur_hashtags)
-        author_num_followers_list.append(curr_author_num_followers)
+        label_list.append(curr_label)
 
-    data = {'id': id_list, 'text': text_list, 'author_id': author_id_list, 'author_followers':author_num_followers_list,
+    data = {'id': id_list, 'text': text_list, 'author_id': author_id_list, 
+            # 'author_followers':author_num_followers_list,
             'retweet_count': retweet_count_list, 'reply_count': reply_count_list,
             'like_count': like_count_list, 'quote_count': quote_count_list,
-            'mentions': mentions_list, 'hashtags': hashtags_list}
+            'mentions': mentions_list, 'hashtags': hashtags_list, 'label': label_list}
 
+    logging.info('Creating dataframe')
     df = pd.DataFrame(data=data)
+    logging.info('Gettting hashtag tfidf')
+    df["hashtags_tfidf"] = hero.tfidf(df['hashtags'])
+    logging.info('Getting sentiment scores')
     df["sentiment_score_pos"] = df['text'].apply(getSentimentScore, type="pos")
     df["sentiment_score_neu"] = df['text'].apply(getSentimentScore, type="neu")
     df["sentiment_score_neg"] = df['text'].apply(getSentimentScore, type="neg")
     df["sentiment_score_comp"] = df['text'].apply(getSentimentScore, type="compound")
-    df["hashtags_tfidf"] = hero.tfidf(df['hashtags'])
     return df
 
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--datadir', '-d', dest='datadir', type=str, help='Directory to store data', default="../data")
+    parser.add_argument('--datadir', '-d', dest='datadir', type=str, help='Directory to store data', default="../data/raw_data/")
+    parser.add_argument('--outdir', '-o', dest='outdir', type=str, help='Directory to store data', default="../data")
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO, filemode='a', format='%(levelname)s - %(message)s')
@@ -111,7 +124,7 @@ if __name__=="__main__":
     start = time.time()
 
     df = preprocess_tweets(args.datadir)
-    outpath = os.path.join(args.datadir, 'all_data_preprocessed.tsv')
+    outpath = os.path.join(args.outdir, 'all_data_preprocessed.tsv')
     df.to_csv(outpath, sep='\t', quoting=csv.QUOTE_NONNUMERIC, index=False)
     
     end = time.time()
